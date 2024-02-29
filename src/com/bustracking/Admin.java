8309e79f7ec3364d6fcc73d6085f1ce01cf98139
Yocanantham <yocanantham@gmail.com>
2024-02-29 14:54:47 +0530

    Modified!

--- src/com/bustracking/Admin.java
+++ src/com/bustracking/Admin.java
@@ -1,4 +1,50 @@
 package com.bustracking;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class Admin {
+    Connection con;
+
+
+    public void admin(String adminid, String adminpassword) {
+        do{
+            con = Mysqlconnection.createDBconnection();
+            String now="select COUNT(mobile) from user";
+            try {
+                PreparedStatement pstm= con.prepareStatement(now);
+                ResultSet result= pstm.executeQuery();
+               while(result.next())
+                {
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    System.out.println("Total no registered users till now : " + result.getInt(1));
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    System.out.println();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            String query = "select mobile, emailid from user";
+            System.out.println("+----------------------+--------------------------------+");
+            System.out.format("| %-20s | %-30s |\n", "MOBILE", "EMAILID");
+            System.out.println("+----------------------+--------------------------------+");
+            try {
+                PreparedStatement pstm = con.prepareStatement(query);
+                ResultSet result = pstm.executeQuery();
+                while (result.next()) {
+                    System.out.format("| %-20s | %-30s |\n",
+                            result.getString(1),
+                            result.getString(2));
+                    System.out.println("+----------------------+--------------------------------+");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }while(!adminid.equals("admin123") || !adminpassword.equals("ADMIN123"));
+//        else
+//        {
+//            System.out.println("Invalid Admin");
+//        }
+    }
 }
